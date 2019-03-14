1de4d1253d4dc6b36f18c7c09678d499b671696a
Marcin Michalski <mmichal2@pantera.robotyka.ia.pw.edu.pl>
2019-03-14 18:20:41 +0100

    a

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mmichal2/anro-litka_michalski/catkin_ws/src
+# Build directory: /home/mmichal2/anro-litka_michalski/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("begginer_tutorials")
+subdirs("beginner_tutorials")
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/mmichal2/anro-litka_michalski/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/mmichal2/anro-litka_michalski/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/mmichal2/anro-litka_michalski/catkin_ws/devel;/opt/ros/melodic" != "" else []
