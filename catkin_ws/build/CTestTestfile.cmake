21c825cede6b8ff8bfc4a06266a0db9e6b66dc12
RafLit <omegalul927@gmail.com>
2019-03-24 13:37:47 +0100

    working repo cleanup

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mmichal2/anro-litka_michalski/catkin_ws/src
-# Build directory: /home/mmichal2/anro-litka_michalski/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("begginer_tutorials")
-subdirs("beginner_tutorials")
--- catkin_ws/build/begginer_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mmichal2/anro-litka_michalski/catkin_ws/src/begginer_tutorials
-# Build directory: /home/mmichal2/anro-litka_michalski/catkin_ws/build/begginer_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
