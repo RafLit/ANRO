8de56582cff3e1055deda23f0581381fa4c52ffb
Marcin Michalski <mmichal2@pantera.robotyka.ia.pw.edu.pl>
2019-03-28 17:39:16 +0100

    params, rqt

--- catkin_ws/src/beginner_tutorials/scripts/steering.py
+++ catkin_ws/src/beginner_tutorials/scripts/steering.py
@@ -4,16 +4,21 @@ import rospy
 from geometry_msgs.msg import Twist 
 rospy.init_node('steering')
 pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+front = rospy.get_param("/front")
+back = rospy.get_param("/back")
+left = rospy.get_param("/left")
+right = rospy.get_param("/right")
+print right
 while not rospy.is_shutdown():
 
     msg = Twist()
     key = click.getchar()
-    if key == 'w':
+    if key == front:
         msg.linear.x = 1
-    elif key == 'a':
-        msg.angular.z = 1
-    elif key == 's':
-        msg.linear.x = -1
-    elif key == 'd':
+    elif key == right:
         msg.angular.z = -1
+    elif key == back:
+        msg.linear.x = -1
+    elif key == left:
+        msg.angular.z = 1
     pub.publish(msg)
